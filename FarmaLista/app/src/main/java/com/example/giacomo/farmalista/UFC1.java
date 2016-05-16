7fa4fd07e20e4a215518018c6197fbdf68fc2a58
massimop1973 <fujisan@libero.it>
2016-05-16 17:28:17 +0200

    modifiche ufc1 di massimo

--- FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC1.java
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC1.java
@@ -1,13 +1,42 @@
 package com.example.giacomo.farmalista;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UFC1 extends AppCompatActivity {
+
+    Button accedi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ufc1);
+
+        accedi = (Button) findViewById(R.id.accedi);
+
+
+        accedi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // per creare e sucessivamente lanciare la nuova activity
+                Intent intent = new Intent(Ufc1.this, Ufc11.class);
+
+                /*
+                // per passare dati da una activity ad un'altra
+                Bundle bundle = new Bundle();
+                bundle.putString("stringa", stringa.getText().toString());
+                bundle.putString("numero", numero.getText().toString());
+                intent.putExtras(bundle);
+                */
+
+                // faccio partire la nuova activity
+                startActivity(intent);
+                // distruggo quella attuale, non è necessario farlo sempre
+                finish();
+            }
+        });
     }
 }
