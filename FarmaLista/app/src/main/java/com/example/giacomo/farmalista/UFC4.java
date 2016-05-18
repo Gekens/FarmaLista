9851a986940eec3c9fe5fe6a291b742812499262
Gekens <giacomozancan@gmail.com>
2016-05-18 10:55:45 +0200

    UFC4

---
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC4.java
@@ -0,0 +1,16 @@
+package com.example.giacomo.farmalista;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Giacomo on 18/05/2016.
+ */
+public class UFC4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ufc4);
+    }
+}
