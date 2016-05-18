6cc14cfc17ebdd119e4de753348063629696d8a2
massimop1973 <fujisan@libero.it>
2016-05-18 10:36:04 +0200

    modifica ufc2 inserito lancio actyvity ufc13 privacy

--- FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC2.java
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC2.java
@@ -1,13 +1,28 @@
 package com.example.giacomo.farmalista;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class UFC2 extends AppCompatActivity {
+
+    TextView privacy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ufc2);
+
+        privacy = (TextView) findViewById(R.id.privacy);
+
+        privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UFC2.this, UFC13.class);
+                startActivity(intent);
+            }
+        });
     }
 }
