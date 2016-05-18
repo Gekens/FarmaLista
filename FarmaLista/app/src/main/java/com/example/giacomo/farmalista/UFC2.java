20575d126a1b1887431a850d474441cb8be10459
massimop1973 <fujisan@libero.it>
2016-05-18 12:01:02 +0200

    modificato ufc2 inserendo gestione registrazione

--- FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC2.java
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC2.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class UFC2 extends AppCompatActivity {
 
-    TextView privacy;
+    TextView privacy, email, password, confermaPassword;
+    Button registrati;
+    String emailString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,34 @@ public class UFC2 extends AppCompatActivity {
         setContentView(R.layout.activity_ufc2);
 
         privacy = (TextView) findViewById(R.id.privacy);
+        email = (TextView) findViewById(R.id.email);
+        password = (TextView) findViewById(R.id.password);
+        confermaPassword = (TextView) findViewById(R.id.confermaPassword);
+        registrati = (Button) findViewById(R.id.registrati);
 
         privacy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(UFC2.this, UFC13.class);
                 startActivity(intent);
+            }
+        });
+
+        registrati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (email.getText().toString().length() != 0) {
+                    emailString = email.getText().toString();
+                } else {
+                    Toast.makeText(UFC2.this, "Inserisci la mail", Toast.LENGTH_SHORT).show();
+                }
+
+                if (password.getText().toString().length() != 0
+                        && password.getText().toString().equals(confermaPassword.getText().toString())) {
+                    passwordString = password.getText().toString();
+                } else {
+                    Toast.makeText(UFC2.this, "Le password non coincidono", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
