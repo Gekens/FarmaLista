f4189b6976cfbb71faf6d5227d79e1a3629174ce
massimop1973 <fujisan@libero.it>
2016-05-18 12:41:35 +0200

    inserito gestione registrati

--- FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC2.java
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC2.java
@@ -3,16 +3,22 @@ package com.example.giacomo.farmalista;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class UFC2 extends AppCompatActivity {
 
     TextView privacy, email, password, confermaPassword;
     Button registrati;
-    String emailString, passwordString;
+    String emailString = null, passwordString = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,7 @@ public class UFC2 extends AppCompatActivity {
         privacy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // lancio l'activity gestione privacy, non chiuso questa activity dove poi devo ritornare
                 Intent intent = new Intent(UFC2.this, UFC13.class);
                 startActivity(intent);
             }
@@ -36,18 +43,46 @@ public class UFC2 extends AppCompatActivity {
         registrati.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // verifico che la mail sia valida
                 if (email.getText().toString().length() != 0) {
                     emailString = email.getText().toString();
                 } else {
                     Toast.makeText(UFC2.this, "Inserisci la mail", Toast.LENGTH_SHORT).show();
                 }
 
+                // verifico di aver inserito le password e se queste coincidono
                 if (password.getText().toString().length() != 0
                         && password.getText().toString().equals(confermaPassword.getText().toString())) {
                     passwordString = password.getText().toString();
                 } else {
                     Toast.makeText(UFC2.this, "Le password non coincidono", Toast.LENGTH_SHORT).show();
                 }
+
+                // se la mail e le password sono state correttamente inserite ...
+                if (emailString != null && passwordString != null) {
+                    // genero il json che verrà salvato nel database
+
+                    JSONObject obj = new JSONObject();
+                    try {
+                        obj.put("email", emailString);
+                        obj.put("password", passwordString);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    Log.d("json",obj.toString());
+
+
+
+
+
+                    // lancio l'activity UFC3
+                    Intent intent = new Intent(UFC2.this, UFC3.class);
+                    startActivity(intent);
+                    finish();
+                }
+
             }
         });
     }
