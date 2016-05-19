5b359ec6cd2296c1b43c76e81a9713b8858ddca0
MicheleDeRussi <michele.derussi@gmail.com>
2016-05-19 13:08:30 +0200

    a

---
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC12.java
@@ -0,0 +1,38 @@
+package com.example.giacomo.farmalista;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by Utente on 19/05/2016.
+ */
+public class UFC12 extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder vBuilder = new AlertDialog.Builder(getActivity());
+        vBuilder.setTitle("il medicinale" + nomeMedicinale + "finirà tra" + giorniRimanenti + "giorni");
+        vBuilder.setPositiveButton("Invia", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        vBuilder.setNegativeButton("Annulla", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        return vBuilder.create();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+}
