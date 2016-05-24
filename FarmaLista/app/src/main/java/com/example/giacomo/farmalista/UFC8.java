0c001f0a5680505c54143276a79e98403b43f86d
Samuele <samuele.pretto@gmail.com>
2016-05-24 22:40:44 +0200

    Sistemati collegamenti UFC8

--- FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC8.java
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC8.java
@@ -3,6 +3,8 @@ package com.example.giacomo.farmalista;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.app.FragmentManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
@@ -16,11 +18,13 @@ import android.view.View;
 
 //test di gestione della Toolbar per possibile Main Activity dove si agganceranno tutti i fragment.
 
-public class UFC8 extends ActionBarActivity {
+public class UFC8 extends ActionBarActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     //istanzio il fragment e il manager dello stesso, l'obiettivo è che nell'UFC 8 all'inizio ci sia la Lista dei Medicinali.
     Fragment_UFC3 lista_medicinali;
     Fragment_UFC4 profilo_utente;
+    Fragment_UFC6 inserisci_medicinale;
+    Fragment_UFC7 lista_contatti;
     FragmentManager vManager;
     ActionBarDrawerToggle actionBarDrawerToggle;
     DrawerLayout drawerLayout;
@@ -39,6 +43,8 @@ public class UFC8 extends ActionBarActivity {
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,myToolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.view_nav);
+        navigationView.setNavigationItemSelectedListener(this);
 
 
         vManager = getFragmentManager();
@@ -80,6 +86,37 @@ public class UFC8 extends ActionBarActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_utente) {
+            profilo_utente=new Fragment_UFC4();
+            FragmentTransaction vTransaction = vManager.beginTransaction();
+            vTransaction.replace(R.id.container,profilo_utente,"pu");
+            vTransaction.commit();
+        } else if (id == R.id.nav_insertMed) {
+            inserisci_medicinale=new Fragment_UFC6();
+            FragmentTransaction vTransaction = vManager.beginTransaction();
+            vTransaction.replace(R.id.container,inserisci_medicinale,"im");
+            vTransaction.commit();
+        } else if (id == R.id.nav_contatti) {
+            lista_contatti=new Fragment_UFC7();
+            FragmentTransaction vTransaction = vManager.beginTransaction();
+            vTransaction.replace(R.id.container,lista_contatti,"lc");
+            vTransaction.commit();
+        } else if (id == R.id.nav_noteLeg) {
+            //Intent vIntent = new Intent(UFC3.this, UFC13.class);
+            //startActivity(vIntent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_container);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     }
