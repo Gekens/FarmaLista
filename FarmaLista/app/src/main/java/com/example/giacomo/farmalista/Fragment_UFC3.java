d0aa5858d628c2535ce16fb8bafea9ef3187d30d
Samuele <samuele.pretto@gmail.com>
2016-05-24 19:58:32 +0200

    test per la toolbar e menù laterale.

--- FarmaLista/app/src/main/java/com/example/giacomo/farmalista/Fragment_UFC3.java
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/Fragment_UFC3.java
@@ -1,23 +1,52 @@
 package com.example.giacomo.farmalista;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Utente on 24/05/2016.
  */
 public class Fragment_UFC3 extends Fragment {
 
+    ListView mListView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View vView = inflater.inflate(R.layout.activity_ufc3,container,false);
 
+        mListView = (ListView) vView.findViewById(R.id.listView);
+        ArrayList<Medicine> vMedicine = new ArrayList<>();
+
+        MedicineAdapter vAdapter= new MedicineAdapter(vView.getContext(),vMedicine);
+        mListView.setAdapter(vAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+        FloatingActionButton fab = (FloatingActionButton) vView.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Inserisci un nuovo medicinale", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+               // Intent vIntent = new Intent(UFC3.this, UFC6.class);
+               // startActivity(vIntent);
+            }
+        });
         return vView;
     }
 }
--- FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC8.java
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/UFC8.java
@@ -1,15 +1,29 @@
 package com.example.giacomo.farmalista;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 //test di gestione della Toolbar per possibile Main Activity dove si agganceranno tutti i fragment.
 
-public class UFC8 extends AppCompatActivity {
+public class UFC8 extends ActionBarActivity {
+
+    //istanzio il fragment e il manager dello stesso, l'obiettivo è che nell'UFC 8 all'inizio ci sia la Lista dei Medicinali.
+    Fragment_UFC3 lista_medicinali;
+    Fragment_UFC4 profilo_utente;
+    FragmentManager vManager;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +32,25 @@ public class UFC8 extends AppCompatActivity {
 
         //creazione dell'oggetto Toolbar definito nel res/menu/toolbar_menu.xml
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_container);
         setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,myToolbar,R.string.app_name,R.string.app_name);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+
+        vManager = getFragmentManager();
+
+     if (savedInstanceState==null){
+            lista_medicinali=new Fragment_UFC3();
+            FragmentTransaction vTransaction = vManager.beginTransaction();
+           vTransaction.add(R.id.container,lista_medicinali,"lm");
+            vTransaction.commit();
+        }
+        else {
+            lista_medicinali = (Fragment_UFC3) vManager.findFragmentByTag("lm");
+        }
     }
 
     @Override
@@ -34,8 +66,10 @@ public class UFC8 extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.user_profile:
 
-                Intent vIntent = new Intent(UFC8.this, UFC4.class);
-                startActivity(vIntent);
+                profilo_utente=new Fragment_UFC4();
+                FragmentTransaction vTransaction = vManager.beginTransaction();
+                vTransaction.replace(R.id.container,profilo_utente,"pu");
+                vTransaction.commit();
 
                 return true;
 
