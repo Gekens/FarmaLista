d96294559fb3b88c17b42c35e37393c1866777cf
Samuele <samuele.pretto@gmail.com>
2016-05-24 23:17:58 +0200

    Test spinner: riuscito

--- FarmaLista/app/src/main/java/com/example/giacomo/farmalista/Fragment_UFC3.java
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/Fragment_UFC3.java
@@ -1,6 +1,8 @@
 package com.example.giacomo.farmalista;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -20,11 +22,15 @@ import java.util.ArrayList;
 public class Fragment_UFC3 extends Fragment {
 
     ListView mListView;
+    Fragment_UFC6 inserisci_medicinale;
+    Fragment_UFC5 modifica_medicinale;
+    FragmentManager vManager;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View vView = inflater.inflate(R.layout.activity_ufc3,container,false);
+        vManager = getFragmentManager();
 
         mListView = (ListView) vView.findViewById(R.id.listView);
         ArrayList<Medicine> vMedicine = new ArrayList<>();
@@ -34,7 +40,10 @@ public class Fragment_UFC3 extends Fragment {
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                modifica_medicinale=new Fragment_UFC5();
+                FragmentTransaction vTransaction = vManager.beginTransaction();
+                vTransaction.replace(R.id.container,modifica_medicinale,"mm");
+                vTransaction.commit();
             }
         });
         FloatingActionButton fab = (FloatingActionButton) vView.findViewById(R.id.fab);
@@ -43,8 +52,10 @@ public class Fragment_UFC3 extends Fragment {
             public void onClick(View view) {
                 Snackbar.make(view, "Inserisci un nuovo medicinale", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-               // Intent vIntent = new Intent(UFC3.this, UFC6.class);
-               // startActivity(vIntent);
+                inserisci_medicinale=new Fragment_UFC6();
+                FragmentTransaction vTransaction = vManager.beginTransaction();
+                vTransaction.replace(R.id.container,inserisci_medicinale,"im");
+                vTransaction.commit();
             }
         });
         return vView;
