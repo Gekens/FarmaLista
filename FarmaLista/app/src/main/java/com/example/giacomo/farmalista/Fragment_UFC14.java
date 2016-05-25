ba3d84402cb7cca1e9a62be12c1f34630c28ba66
massimop1973 <fujisan@libero.it>
2016-05-25 15:39:26 +0200

    il fragment ufc7 richiama il fragment ufc14

---
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/Fragment_UFC14.java
@@ -0,0 +1,22 @@
+package com.example.giacomo.farmalista;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Utente on 25/05/2016.
+ */
+public class Fragment_UFC14 extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View vView = inflater.inflate(R.layout.activity_ufc14,container,false);
+
+
+        return vView;
+    }
+}
--- FarmaLista/app/src/main/java/com/example/giacomo/farmalista/Fragment_UFC7.java
+++ FarmaLista/app/src/main/java/com/example/giacomo/farmalista/Fragment_UFC7.java
@@ -1,22 +1,41 @@
 package com.example.giacomo.farmalista;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Utente on 24/05/2016.
  */
 public class Fragment_UFC7 extends Fragment {
 
+    Button aggiungi;
+
+    FragmentManager manager;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View vView = inflater.inflate(R.layout.activity_ufc7,container,false);
+        manager = getFragmentManager();
+
+        aggiungi = (Button) vView.findViewById(R.id.btnAggiungi);
+
+        aggiungi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment_UFC14 aggiungiContatto = new Fragment_UFC14();
+                FragmentTransaction vTransaction = manager.beginTransaction();
+                vTransaction.replace(R.id.container,aggiungiContatto,"ac");
+                vTransaction.commit();
+            }
+        });
 
         return vView;
     }
